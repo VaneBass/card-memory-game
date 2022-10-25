caf4d89f6c5551609bb38f22d127dfd5b88bec75
VaneBass <1412109759@qq.com>
2022-10-25 20:59:55 +0800

    项目优化: 用雪碧图加载图片

--- src/components/TheIcon.vue
+++ src/components/TheIcon.vue
@@ -1,17 +1,16 @@
 <template>
-  <n-el tag="img" :src="imgSrc" alt="img" class="img" />
+  <!-- <n-el tag="img" :src="imgSrc" alt="img" class="img" /> -->
+  <n-el tag="svg" class="img">
+    <use :href="`${sprite}#${content}`"></use>
+  </n-el>
 </template>
 
 <script setup>
-import { computed } from "vue";
 import { NEl } from "naive-ui";
+import sprite from "../assets/icons/sprite.svg";
 
 const props = defineProps({
   content: String,
-});
-
-const imgSrc = computed(() => {
-  return new URL(`../assets/${props.content}.svg`, import.meta.url).href;
 });
 </script>
 
@@ -19,7 +18,7 @@ const imgSrc = computed(() => {
 .img {
   width: 80%;
   height: 80%;
-
+  color: black;
   filter: drop-shadow(0px 0px 30px var(--primary-color));
 }
 </style>
